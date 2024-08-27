a7d433ba6c70f6a806715b94d5bc338cdc93eb79
Radoslav Tsanev <tsanev96@gmail.com>
2024-08-27 23:08:00 +0300

    day 1 part 1 refactoring

--- src/day01/01_part.ts
+++ src/day01/01_part.ts
@@ -1,27 +1,55 @@
-// /*
-// --- Day 1: Trebuchet?! ---
-// Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
+import { NumbersText } from "./types";
 
-// You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
+const DEFAULT_NOT_EXISTING_NUMBER = { index: -1, value: 0 };
 
-// Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+function findFirstAndLastNumber(result: NumbersText, curr: string, index: number) {
+  const isNumber = Number(curr);
 
-// You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
+  if (!isNumber) { return result }
 
-// As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
+  const { first } = result;
+  const currentNumber = { index, value: isNumber };
 
-// The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+  if (first.index > index || first.index === -1) {
+    result.first = currentNumber
+  }
 
-// For example:
+  result.last = currentNumber
 
-// 1abc2 - 12
-// pqr3stu8vwx - 38
-// a1b2c3d4e5f - 15
-// treb7uchet - 77
-// In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+  return result;
+}
 
-// Consider your entire calibration document. What is the sum of all of the calibration values?
+function getNumbers(text: string) {
+  const { first, last } = text.split('').reduce<NumbersText>(findFirstAndLastNumber, {
+    first: DEFAULT_NOT_EXISTING_NUMBER,
+    last: DEFAULT_NOT_EXISTING_NUMBER
+  });
 
+  return parseInt(`${first.value}${last.value}`, 10);
+};
+
+function getArrayOfNumbers(lines: string[]) {
+  return lines.reduce<number[]>((result, curr) => {
+    const numbers = getNumbers(curr);
+    result.push(numbers)
+    return result;
+  }, []);
+}
+
+function getArraySum(numbers: number[]) {
+  return numbers.reduce((result, curr) => result += curr)
+}
+
+function main(input: string) {
+  const lines = input.split('\n').map(line => line.trim());
+  const numbers = getArrayOfNumbers(lines);
+  const result = getArraySum(numbers);
+  return result;
+}
+
+export default main;
+
+/*
 function trebuchet(args: string[]) {
   const input = args[0].split('\n');
 
@@ -54,5 +82,4 @@ export function extractNumbersFromText(text: string) {
 
   return numbers;
 }
-
-export default trebuchet;
+*/
--- src/day01/index.test.ts
+++ src/day01/index.test.ts
@@ -1,28 +1,43 @@
 import { describe, it, expect } from "@jest/globals";
-import trebuchet from "./01_part";
+import main from "./01_part";
+import main2 from './02_part';
 import { readFileSync } from "fs";
 
 describe('Day 1, part 1', () => {
-  it('Given case', () => {
+  test('Given case', () => {
     const input = `1abc2
                       pqr3stu8vwx
                       a1b2c3d4e5f
                       treb7uchet`;
-    expect(trebuchet([input])).toBe(142);
+    expect(main(input)).toBe(142);
   });
 
-  it("should return 10", () => {
-    expect(trebuchet(["dsa2sd"])).toStrictEqual(22)
+  test("should return 10", () => {
+    expect(main("dsa2sd")).toStrictEqual(22)
   })
 
-  it("should return 92", () => {
-    expect(trebuchet(["ds9a2sd"])).toStrictEqual(92)
+  test("should return 92", () => {
+    expect(main("ds9a2sd")).toStrictEqual(92)
   })
 
-  it('should test real input', () => {
-    const data = readFileSync(__dirname + '/input_1.txt', 'utf8');
-    const sum = trebuchet([data]);
+  test('should test real input', () => {
+    const data = readFileSync(__dirname + '/input.txt', 'utf8');
+    const sum = main(data);
     expect(sum).toBe(55488);
   });
 
 });
+
+describe("Day 2, part 2", () => {
+  test("Given case", () => {
+    const input = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+    expect(main2(input)).toBeUndefined();
+  })
+
+})
